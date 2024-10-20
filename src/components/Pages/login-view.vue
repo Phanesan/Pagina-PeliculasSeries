f084330cdf8b4bf704e15845c1af5e7d43f0ed3d
Vecs <166658809+Vecs09@users.noreply.github.com>
2024-10-20 15:18:18 -0700

    Ignorar(redireccion en home)

--- src/components/Pages/login-view.vue
+++ src/components/Pages/login-view.vue
@@ -1,6 +1,8 @@
 <template>
   <p>Login View</p>
-  <button @click="$emit('changePage', 'Home')">Home</button>
+  <button @click="$emit('changePage', 'DetailArtist')">Home</button>
+  <button @click="$emit('changePage', 'detailCategory')">Category</button>
+
 </template>
 
 <script>
